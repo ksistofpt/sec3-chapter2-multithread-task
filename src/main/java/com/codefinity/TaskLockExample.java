10c9cb93e3b365a45179cd6dcdd21f266fae7612
ksistofpt <162743912+ksistofpt@users.noreply.github.com>
2025-04-27 07:59:16 +0000

    completed Challenge Lock and Condition

--- src/main/java/com/codefinity/TaskLockExample.java
+++ src/main/java/com/codefinity/TaskLockExample.java
@@ -10,19 +10,55 @@ public class TaskLockExample {
     private int counter = 1;
 
     public void redLight() {
-        // TODO: Implement the logic to ensure that the red light is activated.
+        // Implement the logic to ensure that the red light is activated.
+        lock.lock(); // Захоплює блокування
+        try {
+            while (counter != 1) {
+                condition.await(); // Очікує, поки counter не стане 1
+            }
+            System.out.println("red light");
+            counter ++;
+            condition.signalAll(); // // Сповіщає інші потоки, що метод redLight завершився
+        } catch  (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Відновлює статус переривання потоку
+        } finally {
+            lock.unlock(); // Звільняє блокування
+        }
+
+
         // This method should be called first in the sequence.
         // Ensure that other lights do not activate until `redLight()` is completed.
     }
 
     public void yellowLight() throws InterruptedException {
-        // TODO: Implement the logic to ensure that the yellow light is activated after redLight().
+        // Implement the logic to ensure that the yellow light is activated after redLight().
+        lock.lock();
+        try {
+            while (counter != 2) {
+                condition.await();
+            }
+            System.out.println("yellow light");
+            counter ++;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
         // This method should only proceed if `redLight()` has completed.
         // Handle any synchronization needed to ensure that `yellowLight()` runs in the correct order.
     }
 
     public void greenLight() throws InterruptedException {
-        // TODO: Implement the logic to ensure that the green light is activated after `yellowLight()`.
+        // Implement the logic to ensure that the green light is activated after `yellowLight()`.
+        lock.lock();
+        try {
+            while (counter != 3) {
+                condition.await();
+            }
+            System.out.println("green light");
+            // Немає потреби збільшувати лічильник або сповіщати інші потоки."
+        } finally {
+            lock.unlock();
+        }
         // This method should only proceed if `yellowLight()` has completed.
         // Ensure that `greenLight()` activates only after the proper sequence of lights.
     }
